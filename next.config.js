81b853e89a8ee8ae7a5028b35e3f084b49b32996
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2024-08-04 00:32:24 +0400

    SVGR fixes

--- next.config.js
+++ next.config.js
@@ -9,7 +9,14 @@ const nextConfig = {
     webpack: (config) => {
         config.module.rules.push({
             test: /\.svg$/,
-            use: ['@svgr/webpack'],
+            use: [
+                {
+                    loader: '@svgr/webpack',
+                    options: {
+                        icon: true,
+                    },
+                },
+            ],
         });
 
         return config;
