82b719d42c1c4b4d2585e63d21a43d9febce7158
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2024-08-04 04:57:49 +0400

    Tokenomics fixes

--- src/components/home/Tokenomics/Tokenomics.tsx
+++ src/components/home/Tokenomics/Tokenomics.tsx
@@ -12,10 +12,10 @@ const Tokenomics: FC = () => {
             <div className={styles.inner}>
                 <Title className={styles.title} size={'big'}>Tokenomics</Title>
                 <ul className={styles.list}>
-                    <li className={styles.item}>Taxes<br/>0/0</li>
-                    <li className={styles.item}>Mint&Freeze<br/>revoked</li>
-                    <li className={styles.item}>Liquidity<br/>burned</li>
-                    <li className={styles.item}>Total supply<br/>10 billion</li>
+                    <li className={styles.item}>Taxes<br/> 0/0</li>
+                    <li className={styles.item}>Mint&Freeze<br/> revoked</li>
+                    <li className={styles.item}>Liquidity<br/> burned</li>
+                    <li className={styles.item}>Total supply<br/> 10 billion</li>
                 </ul>
             </div>
         </Wrapper>
