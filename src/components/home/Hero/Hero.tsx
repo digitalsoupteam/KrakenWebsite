016f04ab763e58f83ca9c370b006543fd644bccf
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2024-08-04 04:52:08 +0400

    Hero animation

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,5 +1,6 @@
-import React, {FC} from 'react';
+import React, {FC, useRef} from 'react';
 import Image from "next/image";
+import {motion, useScroll, useTransform, MotionValue} from 'framer-motion';
 
 import {Wrapper} from "@/components/layout";
 import {CopyLink, Socials} from "@/components/common";
@@ -7,15 +8,24 @@ import {CopyLink, Socials} from "@/components/common";
 import styles from './Hero.module.css';
 
 const Hero: FC = () => {
+    const targetRef = useRef(null);
+    const {scrollYProgress} = useScroll({target: targetRef});
+
+    const distanceDivider = 0.5;
+    const useParallax = (value: MotionValue, distance: number) => {
+        return useTransform(value, [0, 1], [0, distance / distanceDivider]);
+    };
+
     return <section className={styles.root}>
         <Wrapper>
-            <div className={styles.inner}>
-                <Image className={styles.logo} src={'/kraken.png'} width="300" height="100" alt={'kraken logo'} />
+            <motion.div className={styles.inner} style={{y: useParallax(scrollYProgress, 100)}}>
+                <Image className={styles.logo} src={'/kraken.png'} width="300" height="100" alt={'kraken logo'}/>
 
-                <CopyLink text={'This will be the address of the token'} copyText={'This will be the address of the token'} isLarge={true} />
+                <CopyLink text={'This will be the address of the token'}
+                          copyText={'This will be the address of the token'} isLarge={true}/>
 
-                <Socials />
-            </div>
+                <Socials/>
+            </motion.div>
         </Wrapper>
     </section>
 };
