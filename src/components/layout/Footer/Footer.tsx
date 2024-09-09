9ee454d68faca8585f056fa505edfd6f90411797
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2024-09-09 19:53:20 +0400

    Footer update

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -10,51 +10,67 @@ import {Button} from "@/components/ui";
 import TelegramIcon from '/public/images/telegram.svg';
 
 import styles from './Footre.module.css';
+import {Socials} from "@/components/common";
 
 const Footer: FC = () => {
-    const menuItems = [
+    const primaryNav = [
         {
-            name: 'About',
+            name: 'About KRAKEN',
             link: '/#about',
         },
         {
-            name: 'Tokenomics',
-            link: '/#tokenomics',
+            name: 'Buy $KRAKEN',
+            link: '/#',
         },
         {
-            name: 'Nft',
-            link: '#',
+            name: 'WhitePaper',
+            link: '/#',
         },
         {
-            name: 'Stake',
-            link: '/staking/',
+            name: 'Реферальная программа',
+            link: '/#',
+        },
+    ];
+
+    const secondaryNav = [
+        {
+            name: 'Media Kit',
+            link: '/#',
+        },
+        {
+            name: 'Privacy Policy',
+            link: '/#',
+        },
+        {
+            name: 'Terms and Condition',
+            link: '/#',
         },
     ];
 
     return <footer className={styles.root}>
-        <section className={styles.kraken}>
-            <motion.div
-                className={styles.krakenInner}
-                initial="hidden"
-                whileInView="visible"
-                viewport={{once: true}}
-                transition={{
-                    delay: 0.5,
-                    duration: 0.8,
-                    ease: 'easeInOut',
-                }}
-                variants={{
-                    hidden: {y: '100%'},
-                    visible: {y: 0},
-                }}
-            >
-                <div className={styles.krakenImage}>
-                    <Image src={'/images/kraken.png'} width='251' height='245' alt=''/>
-                </div>
-                <h2 className={styles.title}>
-                    Kraken on solana
-                </h2>
-            </motion.div>
+        <section className={styles.topImage}>
+            {/*<motion.div*/}
+            {/*    className={styles.krakenInner}*/}
+            {/*    initial="hidden"*/}
+            {/*    whileInView="visible"*/}
+            {/*    viewport={{once: true}}*/}
+            {/*    transition={{*/}
+            {/*        delay: 0.5,*/}
+            {/*        duration: 0.8,*/}
+            {/*        ease: 'easeInOut',*/}
+            {/*    }}*/}
+            {/*    variants={{*/}
+            {/*        hidden: {y: '100%'},*/}
+            {/*        visible: {y: 0},*/}
+            {/*    }}*/}
+            {/*>*/}
+            {/*    <div className={styles.krakenImage}>*/}
+            {/*        <Image src={'/images/kraken.png'} width='251' height='245' alt=''/>*/}
+            {/*    </div>*/}
+            {/*    <h2 className={styles.title}>*/}
+            {/*        Kraken on solana*/}
+            {/*    </h2>*/}
+            {/*</motion.div>*/}
             <motion.div
                 initial="hidden"
                 whileInView="visible"
@@ -76,7 +92,7 @@ const Footer: FC = () => {
                 <nav className={styles.nav}>
                     <ul className={styles.navList}>
                         {
-                            menuItems && menuItems.length && menuItems.map(menuItem => (
+                            primaryNav && primaryNav.length && primaryNav.map(menuItem => (
                                 <li key={menuItem.name}>
                                     <Link className={styles.navLink} href={menuItem.link}>{menuItem.name}</Link>
                                 </li>
@@ -85,10 +101,24 @@ const Footer: FC = () => {
                     </ul>
                 </nav>
 
-                <Button href={'https://t.me/thekrakencoin'} target="_blank">
-                    <TelegramIcon/>
-                    Telegram
-                </Button>
+                <div className={styles.branding}>
+                    <Link className={styles.logo} href={"/"}>
+                        <Image className={styles.logoImage} src="/images/logo.png" width="246" height="40" alt={""}/>
+                    </Link>
+                    <Socials/>
+                </div>
+
+                <nav className={styles.nav}>
+                    <ul className={styles.navList}>
+                        {
+                            secondaryNav && secondaryNav.length && secondaryNav.map(menuItem => (
+                                <li key={menuItem.name}>
+                                    <Link className={styles.navLink} href={menuItem.link}>{menuItem.name}</Link>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </nav>
             </div>
         </Wrapper>
     </footer>
